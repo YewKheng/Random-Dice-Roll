1a6d2aad4bbab6c12fbeca44dfa72a96a40f4be7
Yew Kheng Kok <144677450+YewKheng@users.noreply.github.com>
2023-09-29 18:56:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const randomNumber1 = Math.floor(Math.random() * 6) + 1; //Random roll 1-6 for player 1
 const randomNumber2 = Math.floor(Math.random() * 6) + 1; //Random roll 1-6 for player 2
-const randomDicePng1 = "dice" + randomNumber1 + ".png"; //String + random 1-6 + string to get dice1.png - dice6.png for player 1
-const randomDicePng2 = "dice" + randomNumber2 + ".png"; //String + random 1-6 + string to get dice1.png - dice6.png for player 2
-const randomImageSource1 = "images/" + randomDicePng1; //Set path to be = images/dice1.png - images/dice6.png for player 1
-const randomImageSource2 = "images/" + randomDicePng2; //Set path to be = images/dice1.png - images/dice6.png for player 2
-
+const randomDicePng1 = "images/" + "dice" + randomNumber1 + ".png"; //String + random 1-6 + string to get dice1.png - dice6.png for player 1
+const randomDicePng2 = "images/" + "dice" + randomNumber2 + ".png"; //String + random 1-6 + string to get dice1.png - dice6.png for player 2
 const image1 = document.querySelectorAll("img")[0]; //Set dice image for player 1 to be in [0]
 const image2 = document.querySelectorAll("img")[1]; //Set dice image for player 2 to be in [1]
-image1.setAttribute("src", randomImageSource1); //Set src of image for player 1
-image2.setAttribute("src", randomImageSource2); //Set src of image for player 2
+image1.setAttribute("src", randomDicePng1); //Set src of image for player 1
+image2.setAttribute("src", randomDicePng2); //Set src of image for player 2
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins !!!";
